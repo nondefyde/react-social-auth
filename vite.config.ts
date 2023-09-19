2e9de77484a44b28b1752643d42d3f52a51f922f
dapperwalze <dapperwalze@Walzes-MacBook-Pro.local>
2023-09-19 22:40:27 +0100

    feat: create react component library and deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
-
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+import EsLint from 'vite-plugin-linter'
+import tsConfigPaths from 'vite-tsconfig-paths'
+const { EsLinter, linterPlugin } = EsLint
+import * as packageJson from './package.json'
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig((configEnv) => ({
+  plugins: [
+    react(),
+    tsConfigPaths(),
+    linterPlugin({
+      include: ['./src}/**/*.{ts,tsx}'],
+      linters: [new EsLinter({ configEnv })],
+    }),
+    dts({
+      include: ['src/lib/'],
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve('src', 'lib/index.ts'),
+      name: 'ReactSocialAuth',
+      formats: ['es', 'umd'],
+      fileName: (format) => `react-social-auth.${format}.js`,
+    },
+    rollupOptions: {
+      external: [...Object.keys(packageJson.peerDependencies)],
+    },
+  },
+}))

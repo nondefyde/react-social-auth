2d93728114016019ca104cd6a3b26031d4a019f4
dapperwalze <dapperwalze@Walzes-MacBook-Pro.local>
2023-09-21 07:19:20 +0100

    chore: updated tsconfig and vite congig files

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ import * as packageJson from './package.json'
 export default defineConfig((configEnv) => ({
   plugins: [
     dts({
-      include: ['src/lib/'],
+      include: ['src/lib/index.d.ts'],
     }),
     react(),
     tsConfigPaths(),

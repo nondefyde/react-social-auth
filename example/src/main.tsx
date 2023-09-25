90ab7f41f4cc2bcc2dc0d1725f48fe55d5c4c075
dapperwalze <dapperwalze@Walzes-MacBook-Pro.local>
2023-09-25 11:44:54 +0100

    fix: trim social oauth for facebook

--- example/src/main.tsx
+++ example/src/main.tsx
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import { FacebookProvider } from '../../package/src/lib/Facebook'
 
 ReactDOM.render(
   <React.StrictMode>
+    <FacebookProvider appId={process.env.RS_FACEBOOK_APP_ID as string}>
       <App />
+    </FacebookProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,6 +1,13 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-    plugins: [react()],
-});
+
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+    return {
+      define: {
+        'process.env.RS_FACEBOOK_APP_ID': JSON.stringify(env.RS_FACEBOOK_APP_ID)
+      },
+      plugins: [react()],
+    }
+  })

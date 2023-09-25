55518e303521c068047baafad695606c210f7db0
dapperwalze <dapperwalze@Walzes-MacBook-Pro.local>
2023-09-25 11:55:32 +0100

    fix: trim social oauth for facebook

--- package/vite.config.ts
+++ package/vite.config.ts
@@ -1,13 +1,13 @@
 import react from '@vitejs/plugin-react';
-import {readFile} from 'node:fs/promises';
-import {defineConfig, UserConfigExport} from 'vite';
+import { readFile } from 'node:fs/promises';
+import { defineConfig, UserConfigExport, loadEnv } from 'vite';
 import dts from 'vite-plugin-dts';
 import * as path from "path";
 
 const App = async (): Promise<UserConfigExport> => {
     let name: string = 'ReactSocialAuth';
 
-    const data: string = await readFile(path.join(__dirname, 'src', 'lib', 'index.ts'), {encoding: 'utf-8'});
+    const data: string = await readFile(path.join(__dirname, 'src', 'lib', 'index.ts'), { encoding: 'utf-8' });
 
     const s = data.split('\n');
 
@@ -16,31 +16,37 @@ const App = async (): Promise<UserConfigExport> => {
             name = x.replace('export default ', '').replace(" ", "");
     }
 
-    return defineConfig({
-        plugins: [
-            react(),
-            dts({
-                insertTypesEntry: true,
-            }),
-        ],
-        build: {
-            lib: {
-                entry: path.resolve(__dirname, 'src/lib/index.ts'),
-                name,
-                formats: ['es', 'umd'],
-                fileName: (format) => `lib.${format}.js`,
+    return defineConfig(({ mode }) => {
+        const env = loadEnv(mode, process.cwd(), '');
+        return {
+            define: {
+                'process.env.RS_FACEBOOK_APP_ID': JSON.stringify(env.RS_FACEBOOK_APP_ID)
             },
-            rollupOptions: {
-                external: ['react', 'react-dom', 'styled-components'],
-                output: {
-                    globals: {
-                        react: 'React',
-                        'react-dom': 'ReactDOM',
-                        'styled-components': 'styled',
+            plugins: [
+                react(),
+                dts({
+                    insertTypesEntry: true,
+                }),
+            ],
+            build: {
+                lib: {
+                    entry: path.resolve(__dirname, 'src/lib/index.ts'),
+                    name,
+                    formats: ['es', 'umd'],
+                    fileName: (format) => `lib.${format}.js`,
+                },
+                rollupOptions: {
+                    external: ['react', 'react-dom', 'styled-components'],
+                    output: {
+                        globals: {
+                            react: 'React',
+                            'react-dom': 'ReactDOM',
+                            'styled-components': 'styled',
+                        },
                     },
                 },
             },
-        },
+        }
     });
 }
 
